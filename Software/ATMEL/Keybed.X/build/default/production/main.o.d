ffcc8992d46131219e0c8d57fd7a9c0bbc718107
Paula M <Paula@Synth.net>
2025-11-12 13:25:47 +0000

    Keyboard code and schematics
    Updated the Schematics and have a mostly functioning keyboard (can't turn off caps lock yet)

---
+++ Software/ATMEL/Keybed.X/build/default/production/main.o.d
@@ -0,0 +1,70 @@
+build/default/production/main.o.d build/default/production/main.o: main.c \
+ /opt/microchip/xc8/v3.10/avr/lib/gcc/avr/5.4.0/include/xc.h \
+ /opt/microchip/xc8/v3.10/avr/lib/gcc/avr/5.4.0/include/cci.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/avr/io.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/avr/sfr_defs.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/inttypes.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/features.h \
+ /opt/microchip/xc8/v3.10/avr/lib/gcc/avr/5.4.0/include/stdint.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/stdint.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/bits/alltypes.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/bits/stdint.h \
+ /opt/microchip/mplabx/v6.25/packs/Microchip/ATmega_DFP/3.3.279/xc8/avr/include/avr/iom644p.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/avr/portpins.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/avr/common.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/avr/fuse.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/avr/lock.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/avr/builtins.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/avr/wdt.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/util/delay.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/util/delay_basic.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/math.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/stdio.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/avr/interrupt.h \
+ /opt/microchip/xc8/v3.10/avr/avr/include/util/setbaud.h
+
+/opt/microchip/xc8/v3.10/avr/lib/gcc/avr/5.4.0/include/xc.h:
+
+/opt/microchip/xc8/v3.10/avr/lib/gcc/avr/5.4.0/include/cci.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/avr/io.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/avr/sfr_defs.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/inttypes.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/features.h:
+
+/opt/microchip/xc8/v3.10/avr/lib/gcc/avr/5.4.0/include/stdint.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/stdint.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/bits/alltypes.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/bits/stdint.h:
+
+/opt/microchip/mplabx/v6.25/packs/Microchip/ATmega_DFP/3.3.279/xc8/avr/include/avr/iom644p.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/avr/portpins.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/avr/common.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/avr/fuse.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/avr/lock.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/avr/builtins.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/avr/wdt.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/util/delay.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/util/delay_basic.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/math.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/stdio.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/avr/interrupt.h:
+
+/opt/microchip/xc8/v3.10/avr/avr/include/util/setbaud.h:
